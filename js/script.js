6d8c2c9a70eba36064241f8c5ccdf5d701836210
Gpeder <gtzyy008@gmail.com>
2023-05-26 10:23:30 -0300

    JavaScript pronto

--- js/script.js
+++ js/script.js
@@ -24,3 +24,48 @@ function ativarProduto(paramentro) {
 }
 
 paramentros.forEach(ativarProduto);
+
+//perguntas
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//galeria
+
+const galeria = document.querySelectorAll(".bicicleta_imagens img");
+const galeriaContainer = document.querySelector(".bicicleta_imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//plugins
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
